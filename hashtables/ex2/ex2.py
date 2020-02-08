c8a435b0f287fb6fd16c54bc98c223386733e7ad
David <digital0david@gmail.com>
2020-02-08 15:30:40 -0500

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,4 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
+#  Hin  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
                         hash_table_remove,
@@ -11,13 +11,38 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+ticketArray = [
+  Ticket("PIT", "ORD" ),
+  Ticket("XNA", "CID" ),
+  Ticket("SFO", "BHM" ),
+  Ticket( "FLG", "XNA" ),
+  Ticket( "NONE", "LAX" ),
+  Ticket( "LAX", "SFO" ),
+  Ticket( "CID", "SLC" ),
+  Ticket( "ORD", "NONE" ),
+  Ticket( "SLC", "PIT" ),
+  Ticket( "BHM", "FLG" )
+]
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in ticketArray: 
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    route = [None] * length
+    arrayLen = 0
+
+    if hash_table_retrieve(hashtable, "NONE"):
+        route[arrayLen] = hash_table_retrieve(hashtable, "NONE")
+
+    while arrayLen < length - 1 and route[arrayLen] is not None:
+        destination = hash_table_retrieve(hashtable, route[arrayLen])
+        
+        arrayLen += 1
+        route[arrayLen] = destination
+        
 
     return route
+
+print(reconstruct_trip(ticketArray, 10))
